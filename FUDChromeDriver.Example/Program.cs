ebd8f3ec5a8849970e87249fdda518db0e4643fd
ZygoteCode <zygotecode@proton.me>
2025-06-06 14:24:29 +0200

    First commit

---
+++ FUDChromeDriver.Example/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FUDChromeDriver.Example
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            UndetectedChromeDriver driver = UndetectedChromeDriver.Create(driverExecutablePath: "C:\\Users\\Z\\Desktop\\FUDChromeDriver\\FUDChromeDriver\\bin\\x64\\Release\\net9.0\\chromedriver.exe", browserExecutablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
+            driver.GoToUrl("https://bot.sannysoft.com");
+        }
+    }
+}
